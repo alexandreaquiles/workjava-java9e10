3db5b79924e917967aa0ed110833bb4b85fc6ee6
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-04-11 06:52:09 -0300

    corrige copia-jars.sh

--- code/imc-modulos/copia-jars.sh
+++ code/imc-modulos/copia-jars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp imc-domain/target/imc-domain-1.0.jar ~/workjava/code/libs
-cp imc-cli/target/imc-cli-1.0.jar ~/workjava/code/libs
-cp imc-desktop/target/imc-desktop-1.0.jar ~/workjava/code/libs
-cp imc-application/target/imc-application-1.0.jar ~/workjava/code/libs
-cp imc-xml/target/imc-xml-1.0.jar ~/workjava/code/libs
+cp imc-domain/target/imc-domain-1.0.jar ../libs
+cp imc-cli/target/imc-cli-1.0.jar ../libs
+cp imc-desktop/target/imc-desktop-1.0.jar ../libs
+cp imc-application/target/imc-application-1.0.jar ../libs
+cp imc-xml/target/imc-xml-1.0.jar ../libs
 
